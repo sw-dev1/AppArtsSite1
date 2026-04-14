fe2ae58bc9b0f369c191d0c2f70383d013228244
sw-dev1 <sw-dev@outlook.com>
2026-04-14 17:12:32 +0300

    Correct patents URLs #WSAA-1

--- composeApp/src/commonMain/kotlin/com/apparts/sites/appartssite1/ui/screens/PatentsData.kt
+++ composeApp/src/commonMain/kotlin/com/apparts/sites/appartssite1/ui/screens/PatentsData.kt
@@ -14,20 +14,20 @@ val patents = listOf(
         title = "User management for identification systems",
         number = "10 2022 105 680.5",
         description = "The invention consists of the idea about how to incorporate existing user/account management subsystems into complete identification systems by retrieving and implementing user/account IDs throughout the identification, authentication and authorization workflows. The idea enables component providers to build complete systems by reusing existing account management subsystems.",
-        url = "https://register.dpma.de/DPMAregister/pat/register?lang=en&fromSprachWechselLink"
+        url = "https://register.dpma.de/DPMAregister/pat/register?AKZ=1020221056805"
     ),
     Patent(
         id = 1,
         title = "Satisfying Contradictory Requirements",
         number = "10 2022 105 685.6",
         description = "Function/Goal: create a system where the same components interact differently in order to implement independent applications that place contradictory requirements on the components.",
-        url = "https://register.dpma.de/DPMAregister/pat/register?lang=en&fromSprachWechselLink"
+        url = "https://register.dpma.de/DPMAregister/pat/register?AKZ=1020221056856"
     ),
     Patent(
         id = 2,
         title = "Predict values without measuring",
         number = "10 2022 105 687.2",
         description = "Function/Goal: Determine/predict strongly varying device-specific values by using common characteristics and relating measured values to the unknown values.",
-        url = "https://register.dpma.de/DPMAregister/pat/register?lang=en&fromSprachWechselLink"
+        url = "https://register.dpma.de/DPMAregister/pat/register?AKZ=1020221056872"
     )
 )
